7419418c829387f107fe0b9f2ca8d6f692272d82
mainyaa <mainya@gmail.com>
2015-04-23 16:07:30 +0900

    fix: code format

--- src/browser/app.js
+++ src/browser/app.js
@@ -1,12 +1,13 @@
-var ipc = require("ipc");
+'use strict';
+var ipc = require('ipc');
 
-document.addEventListener("DOMContentLoaded", function(event) {
-    window.jQuery = require("jquery");
-    var $ = window.jQuery;
+var jQuery = require('jquery');
+var $ = jQuery;
+$().ready(function(event) {
     System.paths = {
         'angular2/*':'/quickstart/angular2/*.js', // Angular
         'rtts_assert/*': '/quickstart/rtts_assert/*.js', //Runtime assertions
-        'jQuery':'/bower_components/jquery/dist/jquery.js', // Angular
+        '$':'/bower_components/jquery/dist/jquery.js', // Angular
         'jQuery.fn.contextMenu':'/bower_components/contextMenu/contextMenu.js', // Angular
         'todo': 'todo.js',
         'firebase/*': '/deps/firebase/*.js',
@@ -18,26 +19,26 @@ document.addEventListener("DOMContentLoaded", function(event) {
         module.main();
     }, console.log.bind(console));
     System.import('jQuery.fn.contextMenu').then(function() {
-        $("html").contextMenu(
-            "menu",
-            ".contextMenu",
+        $('html').contextMenu(
+            'menu',
+            '.contextMenu',
             {
-                "triggerOn": "contextmenu",
-                "afterOpen": function(){
-                    $(".overlayMenu").show();
-                    $(".overlayMenu").on("click", function(event){
+                'triggerOn': 'contextmenu',
+                'afterOpen': function(){
+                    $('.overlayMenu').show();
+                    $('.overlayMenu').on('click', function(event){
                         event.preventDefault();
-                        $("html").contextMenu(
-                            "close",
-                            ".contextMenu",
-                            {"triggerOn": "click"}
+                        $('html').contextMenu(
+                            'close',
+                            '.contextMenu',
+                            {'triggerOn': 'click'}
                         );
                     });
                 }
             }
         );
-        $(".contextmenu-devtools-button").on("click", function(evt){
-            ipc.send("toggleDevTools", evt.offsetX, evt.offsetY);
+        $('.contextmenu-devtools-button').on('click', function(evt){
+            ipc.send('toggleDevTools', evt.offsetX, evt.offsetY);
         });
     }, console.log.bind(console));
 });
